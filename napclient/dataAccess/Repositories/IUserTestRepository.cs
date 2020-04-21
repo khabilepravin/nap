d900a2a4fcb5effbcc33623cd63b3879ea1aa11c
Pravin Khabile <pravin.khabile@leapdev.io>
2020-04-21 14:24:59 +1000

    UserTestRepository implementation

--- napclient/dataAccess/Repositories/IUserTestRepository.cs
+++ napclient/dataAccess/Repositories/IUserTestRepository.cs
@@ -1,10 +1,15 @@
-﻿using System;
+﻿using models;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace dataAccess.Repositories
 {
     public interface IUserTestRepository
     {
+        Task<string> AddAsync(UserTest userTest);
+        Task<IEnumerable<UserTest>> GetByUserIdAsync(string userId);
+        Task<string> UpdateAsync(UserTest userTest);
     }
 }
--- napclient/dataAccess/Repositories/UserTestRepository.cs
+++ napclient/dataAccess/Repositories/UserTestRepository.cs
@@ -1,9 +1,47 @@
 ﻿using dataModel.Repositories;
+using Microsoft.EntityFrameworkCore;
+using models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace dataAccess.Repositories
 {
     public class UserTestRepository : BaseRepository, IUserTestRepository
     {
         public UserTestRepository(IDbContextFactory dbContextFactory) : base(dbContextFactory) { }
+
+        public async Task<string> AddAsync(UserTest userTest)
+        {
+            using(var db = base._dbContextFactory.Create())
+            {
+                userTest.CreatedAt = DateTime.UtcNow;
+                await db.AddAsync(userTest);
+                await db.SaveChangesAsync();
+                return userTest.Id;
+            }
+        }
+
+        public async Task<IEnumerable<UserTest>> GetByUserIdAsync(string userId)
+        {
+            using (var db = base._dbContextFactory.Create())
+            {
+                return await (from ut in db.UserTest
+                       where ut.UserId == userId
+                       select ut).ToListAsync<UserTest>();
+            }
+        }
+
+        public async Task<string> UpdateAsync(UserTest userTest)
+        {
+            using (var db = base._dbContextFactory.Create())
+            {
+                userTest.ModifiedAt = DateTime.UtcNow;
+                db.Update(userTest);
+                await db.SaveChangesAsync();
+                return userTest.Id;
+            }
+        }
     }
 }
