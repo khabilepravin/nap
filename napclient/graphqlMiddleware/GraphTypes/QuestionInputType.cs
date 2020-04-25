789446ba75a314b90e85bbba2aa43bae9017c449
Pravin Khabile <pravin.khabile@leapdev.io>
2020-04-25 15:13:00 +1000

    Question input type and associated mutation code

---
+++ napclient/graphqlMiddleware/GraphTypes/QuestionInputType.cs
@@ -0,0 +1,20 @@
+﻿using GraphQL.Types;
+
+namespace graphqlMiddleware.GraphTypes
+{
+    public class QuestionInputType : InputObjectGraphType
+    {
+        public QuestionInputType()
+        {
+            Name = "QuestionInput";
+            Field<NonNullGraphType<StringGraphType>>("text");
+            Field<StringGraphType>("description");
+            Field<StringGraphType>("questionType");
+            Field<IdGraphType>("testId");
+            Field<IntGraphType>("sequence");
+            Field<StringGraphType>("imageUrl");
+            Field<DateGraphType>("createdAt");
+            Field<IdGraphType>("createdByUser");
+        }
+    }
+}
