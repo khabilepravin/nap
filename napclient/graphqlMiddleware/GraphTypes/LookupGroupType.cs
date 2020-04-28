d46adb4f8173ee66c6b0b19bbc83efc0d4bb0d3e
Pravin Khabile <pravin.khabile@leapdev.io>
2020-04-28 21:20:46 +1000

    Child graph type of lookupvalues in lookup group

--- napclient/graphqlMiddleware/GraphTypes/LookupGroupType.cs
+++ napclient/graphqlMiddleware/GraphTypes/LookupGroupType.cs
@@ -1,15 +1,18 @@
-﻿using GraphQL.Types;
+﻿using dataAccess.Repositories;
+using GraphQL.Types;
 using models;
 
 namespace graphqlMiddleware.GraphTypes
 {
     public class LookupGroupType : ObjectGraphType<LookupGroup>
     {
-        public LookupGroupType()
+        public LookupGroupType(ILookupRepository lookupRepository)
         {
             Field(t => t.Id, type: typeof(IdGraphType));
             Field(t => t.Name);
             Field(t => t.Code);
+            Field<ListGraphType<LookupValueType>>("lookupValues",
+                resolve: context => lookupRepository.GetValuesByGroupAsync(context.Source.Id));
         }
     }
 }
