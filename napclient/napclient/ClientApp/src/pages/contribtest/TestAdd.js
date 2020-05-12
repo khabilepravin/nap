2146b757bda1fa9a1897b6207fa203b9f557032d
Pravin Khabile <pravin.khabile@leapdev.io>
2020-05-12 11:19:52 +1000

    Integration with Yup and graph mutation integration

--- napclient/napclient/ClientApp/src/pages/contribtest/TestAdd.js
+++ napclient/napclient/ClientApp/src/pages/contribtest/TestAdd.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { useFormik } from "formik";
+import { Formik } from "formik";
+import * as Yup from 'yup';
 
 import gql from "graphql-tag";
 import { useMutation } from "@apollo/react-hooks";
@@ -24,7 +25,7 @@ import {
 import Header from "../../components/themecomponents/Header";
 import HeaderTitle from "../../components/themecomponents/HeaderTitle";
 
-const ADD_TODO = gql`
+const ADD_TEST = gql`
   mutation($test: TestInput!) {
     createTest(test: $test) {
       text
@@ -35,24 +36,8 @@ const ADD_TODO = gql`
   }
 `;
 
-const onAddFormSubmit = (event) => {
-  event.preventDefault();
-  console.log("comes here");
-};
-
 const TestAdd = () => {
-  const formik = useFormik({
-    initialValues: {
-      text: '',
-      description: '',
-      subject: '',
-      year: 0,
-    },
-    onSubmit: (values) => {
-     // alert(JSON.stringify(values));
-     
-    },
-  });
+  const [addTest] = useMutation(ADD_TEST);
 
   return (
     <Container fluid>
@@ -77,58 +62,85 @@ const TestAdd = () => {
               </CardTitle>
             </CardHeader>
             <CardBody>
-              <Form onSubmit={formik.handleSubmit}>
-                <FormGroup>
-                  <Label>Name</Label>
-                  <Input
-                    type="text"
-                    name="text"
-                    placeholder="Name"
-                    onChange={formik.handleChange}
-                    value={formik.values.text}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label>Description</Label>
-                  <Input
-                    type="textarea"
-                    name="textarea"
-                    placeholder="Description"
-                    rows="1"
-                    onChange={formik.handleChange}
-                    value={formik.values.description}
-                  />
-                </FormGroup>
-                <FormGroup>
-                  <Label>Subject</Label>
-                  <Input
-                    type="select"
-                    id="exampleCustomSelect"
-                    name="customSelect"
-                    className="mb-3"
-                  >
-                    <option value="">Select Subject</option>
-                    <option>Math</option>
-                    <option>English</option>
-                    <option>Geo</option>
-                  </Input>
-                </FormGroup>
-                <FormGroup>
-                  <Label>Year</Label>
-                  <Input
-                    type="select"
-                    id="exampleCustomSelect"
-                    name="customSelect"
-                    className="mb-3"
-                  >
-                    <option value="">Select Year</option>
-                    <option>3</option>
-                    <option>4</option>
-                    <option>5</option>
-                  </Input>
-                </FormGroup>
-                <Button color="primary">Submit</Button>
-              </Form>
+              <Formik
+                initialValues={{
+                  text: "",
+                  description: "",
+                  subject: "",
+                  year: 3,
+                }}
+                validationSchema={Yup.object({
+                  text: Yup.string()
+                    .max(15, 'Must be 15 characters or less')
+                    .required('Name is required'),
+                  description: Yup.string()
+                    .max(20, 'Must be 20 characters or less')
+                    .required('Description is required'),
+                  subject: Yup.string()                    
+                    .required('Subject is required'),
+                  year: Yup.string()
+                    .required('Year is required')
+                })}
+                onSubmit={(values, { setSubmitting }) => {
+                  addTest({variables: { test: values } });
+                  setSubmitting(false);                  
+                }}
+              >
+                {(formik) => (
+                  <Form onSubmit={formik.handleSubmit}>
+                    <FormGroup>
+                      <Label>Name</Label>
+                      <Input
+                        type="text"
+                        name="text"
+                        placeholder="Name"
+                       {...formik.getFieldProps('text')}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Description</Label>
+                      <Input
+                        type="textarea"
+                        name="description"
+                        placeholder="Description"
+                        rows="1"
+                        {...formik.getFieldProps('description')}
+                      />
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Subject</Label>
+                      <Input
+                        type="select"
+                        id="exampleCustomSelect"
+                        name="subject"
+                        className="mb-3"
+                        {...formik.getFieldProps('subject')}
+                      >
+                        <option value="">Select Subject</option>
+                        <option>Math</option>
+                        <option>English</option>
+                        <option>Geo</option>
+                      </Input>
+                    </FormGroup>
+                    <FormGroup>
+                      <Label>Year</Label>
+                      <Input
+                        type="select"
+                        id="exampleCustomSelect"
+                        name="year"
+                        className="mb-3"
+                        {...formik.getFieldProps('year')}
+                      >
+                        <option value="">Select Year</option>
+                        <option>3</option>
+                        <option>4</option>
+                        <option>5</option>
+                      </Input>
+                    </FormGroup>
+                    <Button color="primary">Submit</Button>
+                  </Form>
+                )}
+              </Formik>
             </CardBody>
           </Card>
         </Col>
