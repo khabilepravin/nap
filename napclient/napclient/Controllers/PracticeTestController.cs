1e489555ca320513cde2895e7472b8c23bcaa8c7
Pravin Khabile <pravin.khabile@leapdev.io>
2020-07-11 16:47:18 +1000

    Text input type of answer endpoint

---
+++ napclient/napclient/Controllers/PracticeTestController.cs
@@ -0,0 +1,24 @@
+﻿using logic;
+using logic.RequestModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace napclient.Controllers
+{
+    public class PracticeTestController : BaseController
+    {
+        private readonly IPracticeTestLogic practiceTestLogic;
+        public PracticeTestController(IPracticeTestLogic practiceTestLogic)
+        {
+            this.practiceTestLogic = practiceTestLogic;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RecordTextAnswer([FromBody]TextAnswerRecord testAnswerRecord)
+        {
+            await this.practiceTestLogic.RecordTextAnswer(testAnswerRecord);
+
+            return Ok();
+        }
+    }
+}
