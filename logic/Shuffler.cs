8577af86d50689a59c84cdc4c24ae399cbff6c4c
Pravin Khabile <pravin.khabile@leapdev.io>
2020-09-08 16:51:28 +1000

    Fix for the shuffle logic

--- logic/Shuffler.cs
+++ logic/Shuffler.cs
@@ -4,26 +4,15 @@ using System.Linq;
 
 namespace logic
 {
-    public static class ThreadSafeRandom
-    {
-        [ThreadStatic] private static Random Local;
-        public static int seed = 0;
-        public static Random ThisThreadsRandom
-        {
-            get { return Local ?? (Local = new Random(seed)); }            
-        }
-    }
-
     public static class Shuffler
     {
         public static IEnumerable<T> Shuffle<T>(IList<T> list, int seed)
-        {
-            ThreadSafeRandom.seed = seed;
+        {            
             int n = list.Count;
             while (n > 1)
             {
                 n--;
-                int k = ThreadSafeRandom.ThisThreadsRandom.Next(n + 1);
+                int k = new Random(seed).Next(n + 1);
                 T value = list[k];
                 list[k] = list[n];
                 list[n] = value;
