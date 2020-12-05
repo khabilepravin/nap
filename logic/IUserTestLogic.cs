bf90b85984f3ac28ae93288ae26fe8059a478e97
Pravin Khabile <pravin.khabile@leapdev.io>
2020-12-05 22:40:47 +1100

    Update logic UserTest logic

--- logic/IUserTestLogic.cs
+++ logic/IUserTestLogic.cs
@@ -9,5 +9,7 @@ namespace logic
     {
         Task<TestProgress> GetTestCompletionPercentage(Guid userTestId);
         Task<UserTest> AddUserTest(UserTest userTest);
+        Task<UserTest> UpdateUserTest(UserTest userTest);
+
     }
 }
